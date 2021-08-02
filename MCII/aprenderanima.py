7f72514634dfd52b2063f75bd246e9f122f1066e
Pedro-Luis <plao2411p@gmail.com>
2021-08-01 23:39:37 -0500

    Animaciones

--- MCII/aprenderanima.py
+++ MCII/aprenderanima.py
@@ -8,26 +8,27 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 
-def pendulum(y, t, b, c):
-    theta, omega = y
-    dydt = [
-        omega,
-        -b*omega - c*np.sin(theta)
-    ]
-    return dydt
+def firstgraphic():
 
+    def pendulum(y, t, b, c):
+        theta, omega = y
+        dydt = [
+            omega,
+            -b*omega*t - c*np.sin(theta)
+        ]
+        return dydt
 
-b = 0.25
-c = 5.0
+    b = 0.25
+    c = 5.0
 
-y0 = [np.pi - 0.1, 0.0]
-t = np.arange(0, 10, 0.02)
+    y0 = [np.pi - 0.1, 0.0]
+    t = np.arange(0, 10, 0.02)
 
-sol = odeint(pendulum, y0, t, args=(b, c))
+    sol = odeint(pendulum, y0, t, args=(b, c))
 
-plt.plot(t, sol[:, 0], 'b', label=r'$\theta(t)$')
-plt.plot(t, sol[:, 1], 'g', label=r'$\omega(t)$')
-plt.legend(loc='best')
-plt.xlabel('t')
-plt.grid()
-plt.savefig('solve-ode.svg')
+    plt.plot(t, sol[:, 0], 'b', label=r'$\theta(t)$')
+    plt.plot(t, sol[:, 1], 'g', label=r'$\omega(t)$')
+    plt.legend(loc='best')
+    plt.xlabel('t')
+    plt.grid()
+    plt.savefig('solve-ode.svg')
